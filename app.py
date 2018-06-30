c1b9d19bf51459d0c335027132f3f3745305a863
Ievgen Ragulin <jvyrus@gmail.com>
2018-06-30 17:32:05 -0400

    set up db connection

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-import settings
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 import os
 
-app = Flask(__name__)
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import inspect
+from sqlalchemy.sql import text
 
 @app.route('/')
 def homepage():
@@ -11,11 +14,15 @@ def homepage():
 
 @app.route('/witness/', methods=["POST"])
 def witness():
-    location = request.json['location']
-    description = request.json['description']
-
+    engine = create_engine('postgres://hqbydtfyklgvdi:e84dcb01868fc31a6c8ccb2926411bf4532a4b4e141ff96637365c9cbce97544@ec2-54-83-15-95.compute-1.amazonaws.com:5432/de97nb9dek9b26')
+    connection = engine.connect()
+    
+    # This is just sample code to show that db connection works
+    cmd = 'select * from subscribers where id > :id'
+    id = 0
+    subscribers = connection.execute(text(cmd), id=id)
+    for subscriber in subscribers: print(subscriber)
     #TODO: Parse location
-
     #TODO: insert into database
     print('Location: ', location)
     print('Description: ', description)
