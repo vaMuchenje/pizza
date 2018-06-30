60a9eaaa89670e5ac2ff8e6ea0fa21d04c11e166
Justin Parker <justin@jpark.tech>
2018-06-30 12:26:22 -0400

    add witness endpoint

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     return "hi"
+
+@app.route('/witness/', methods=["POST"])
+def web_witness():
+    location = request.json['location']
+    description = request.json['description']
+
+    #TODO: Parse location
+
+    #TODO: insert into database
+    print('Location: ', location)
+    print('Description: ', description)
+
+    return jsonify(success=True)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
