bd85252f4cb9e1fe6d713c6a5881130decb0c2fe
Ievgen Ragulin <jvyrus@gmail.com>
2018-06-30 17:42:44 -0400

    Write incident data to db

--- app.py
+++ app.py
@@ -16,19 +16,10 @@ def homepage():
 def witness():
     engine = create_engine('postgres://hqbydtfyklgvdi:e84dcb01868fc31a6c8ccb2926411bf4532a4b4e141ff96637365c9cbce97544@ec2-54-83-15-95.compute-1.amazonaws.com:5432/de97nb9dek9b26')
     connection = engine.connect()
-    
-    # This is just sample code to show that db connection works
-    cmd = 'select * from subscribers where id > :id'
-    id = 0
-    subscribers = connection.execute(text(cmd), id=id)
-    for subscriber in subscribers: print(subscriber)
-    #TODO: Parse location
-    #TODO: insert into database
-    print('Location: ', location)
-    print('Description: ', description)
+    cmd = 'INSERT INTO incidents (address, description, incident_lat, incident_lon) VALUES (:address, :description, :lat, :lon)'
+    # TODO: Parse location
+    connection.execute(text(cmd), address=request.json['location'], description=request.json['location'], lat=0, lon=0)
     return jsonify(success=True)
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
