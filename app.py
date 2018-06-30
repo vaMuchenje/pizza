166d458e7b6f36062b0f9fcba4ce3c6856615a55
Ievgen Ragulin <jvyrus@gmail.com>
2018-06-30 18:33:35 -0400

    add geocoding of address

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy import inspect
 from sqlalchemy.sql import text
+import googlemaps
+from datetime import datetime
 
 @app.route('/')
 def homepage():
@@ -17,8 +19,13 @@ def witness():
     engine = create_engine('postgres://hqbydtfyklgvdi:e84dcb01868fc31a6c8ccb2926411bf4532a4b4e141ff96637365c9cbce97544@ec2-54-83-15-95.compute-1.amazonaws.com:5432/de97nb9dek9b26')
     connection = engine.connect()
     cmd = 'INSERT INTO incidents (address, description, incident_lat, incident_lon) VALUES (:address, :description, :lat, :lon)'
-    # TODO: Parse location
-    connection.execute(text(cmd), address=request.json['location'], description=request.json['location'], lat=0, lon=0)
+    location = request.json['location']
+    gmaps = googlemaps.Client(key='AIzaSyAN_7R660HphAES8oxniHvHa4ymuCz32Jc')
+    geocode_result = gmaps.geocode(location)
+    print(geocode_result[0]['geometry']['location'])
+    lat = geocode_result[0]['geometry']['location']['lat']
+    lon = geocode_result[0]['geometry']['location']['lng']
+    connection.execute(text(cmd), address=location, description=request.json['description'], lat=lat, lon=lon)
     return jsonify(success=True)
 
 if __name__ == '__main__':
