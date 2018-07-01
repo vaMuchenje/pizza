b0b72483feeef6133e8948db6e85344f72d7bb38
Frank Orefice <Frankorefice1997@yahoo.com>
2018-06-30 21:36:28 -0400

    The Twilio application
    Send pictures, location and a description using the Twilio API

--- app.py
+++ app.py
@@ -1,39 +1,36 @@
-from flask import Flask, request, jsonify
+from twilio.rest import Client
+from flask import Flask, request
+from twilio.rest import TwilioRestClient
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio import twiml
+
+account_sid = 'AC0d2d5b287cb88cb9e20c4d21c599d38a'
+auth_token = '9e85d47b90e6ae03275e8447e611f1ee'
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+          .create(
+              body = "This is the Whistle messaging service. Send a picture if you have of any suspected "\
+               "ICE related in your neighborhood. Please include the Location and Description in the same message."\
+                "Make sure to add a semicolon (;) after the location and the description.",
+              from_='+12014705763',
+              to='+19178152736' 
+              )
+
+print(message.sid);
+
 app = Flask(__name__)
-import os
 
-import sqlalchemy
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy import inspect
-from sqlalchemy.sql import text
-import googlemaps
-from datetime import datetime
+@app.route('/sms', methods=['GET', 'POST'])
 
-@app.route('/')
-def homepage():
-    return "hii"
+def sms_reply():
+    resp = MessagingResponse()
 
-# This is needed to Allow CORS
-@app.route('/witness/', methods=['GET'])
-def witness_get():
-    response = jsonify({'some': 'data'})
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    if request.form['NumMedia'] != '0':
+        resp.message("Thank you for the tip!")
 
-@app.route('/witness/', methods=["POST"])
-def witness():
-    engine = create_engine('postgres://hqbydtfyklgvdi:e84dcb01868fc31a6c8ccb2926411bf4532a4b4e141ff96637365c9cbce97544@ec2-54-83-15-95.compute-1.amazonaws.com:5432/de97nb9dek9b26')
-    connection = engine.connect()
-    cmd = 'INSERT INTO incidents (address, description, incident_lat, incident_lon) VALUES (:address, :description, :lat, :lon)'
-    location = request.json['location']
-    gmaps = googlemaps.Client(key='AIzaSyAN_7R660HphAES8oxniHvHa4ymuCz32Jc')
-    geocode_result = gmaps.geocode(location)
-    print(geocode_result[0]['geometry']['location'])
-    lat = geocode_result[0]['geometry']['location']['lat']
-    lon = geocode_result[0]['geometry']['location']['lng']
-    connection.execute(text(cmd), address=location, description=request.json['description'], lat=lat, lon=lon)
-    return jsonify(success=True)
+        return str(resp);
+    
+if __name__ == "__main__":
+    app.run(debug=True);
 
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
